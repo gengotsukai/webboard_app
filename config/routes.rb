114d8e0b001eda0622794ffdf78bdcdce8c80c76
gengotsukai <benioni2005@yahoo.co.jp>
2020-06-24 22:04:57 +0900

    [start]creating board functions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'themes/index'
+  get 'themes/show/:id' => 'themes#show', as: :themes_show
   devise_for :users
   root 'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_24_071411) do
+ActiveRecord::Schema.define(version: 2020_06_24_123924) do
+
+  create_table "themes", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
